434d17865cdf8d336bca4f6bba968cfd3fc3a7e5
HowLonghasThisBin <mlonghas_180000001821@uic.edu.ph>
2025-02-06 15:15:08 +0800

    Added the missing tables in enableRLS() and injectPolicies() function.

--- Migration.py
+++ Migration.py
@@ -8,7 +8,7 @@ from Data.masterdata import health_institution, financial_institution
 
 def createTable():
     Create_table_query="""
-         CREATE TABLE  if not exists HealthInstitution (
+        CREATE TABLE  if not exists HealthInstitution (
             HealthInstitutionID bigint primary key generated always as identity,
             HealthInstitutionName text NOT NULL,
             HealthInstitutionDesc text NOT NULL,
@@ -58,7 +58,7 @@ def createTable():
             updated_at timestamp
         );
             
-        CREATE TABLE if not exists FinancialInstitutionNotes (
+        CREATE TABLE if not exists FinancialInstitutionNote (
             FinancialInstitutionNotesID bigint primary key generated always as identity,
             FinancialInstitutionNotesName text NOT NULL,
             FinancialInstitutionNotesDesc text NOT NULL,
@@ -68,7 +68,7 @@ def createTable():
             updated_at timestamp
         );
             
-        CREATE TABLE if not exists FinancialInstitutionBenefits (
+        CREATE TABLE if not exists FinancialInstitutionBenefit (
             FinancialInstitutionBenefitsID bigint primary key generated always as identity,
             FinancialInstitutionBenefitsName text NOT NULL,
             FinancialInstitutionBenefitsDesc text NOT NULL,
@@ -108,19 +108,19 @@ def createTable():
             updated_at timestamp
         );
         
-        CREATE TABLE if not exists Blogs (
-            BlogsID bigint primary key generated always as identity,
-            BlogsName text NOT NULL,
-            BlogsAuthor text NOT NULL,
+        CREATE TABLE if not exists Blog (
+            BlogID bigint primary key generated always as identity,
+            BlogName text NOT NULL,
+            BlogAuthor text NOT NULL,
             created_at timestamp with time zone DEFAULT now(),
             updated_at timestamp
         );
         
-        CREATE TABLE if not exists BlogsRefLink (
-            BlogsRefLinkID bigint primary key generated always as identity,
-            BlogsRefLink text NOT NULL,
-            BlogsID bigint NOT NULL,
-            FOREIGN KEY (BlogsID) REFERENCES Blogs(BlogsID) ON DELETE CASCADE,
+        CREATE TABLE if not exists BlogRefLink (
+            BlogRefLinkID bigint primary key generated always as identity,
+            BlogRefLink text NOT NULL,
+            BlogID bigint NOT NULL,
+            FOREIGN KEY (BlogID) REFERENCES Blog(BlogID) ON DELETE CASCADE,
             created_at timestamp with time zone DEFAULT now(),
             updated_at timestamp
         );
@@ -134,6 +134,13 @@ def createTable():
             FinancialInstitutionID bigint NULL,
             EventOrganizerID bigint NULL,
             ClinicID bigint NULL,
+            
+            CHECK (
+                (HealthInstitutionID IS NOT NULL)::int +
+                (FinancialInstitutionID IS NOT NULL)::int +
+                (EventOrganizerID IS NOT NULL)::int +
+                (ClinicID IS NOT NULL)::int = 1
+            ),
             
             CONSTRAINT FK_HealthInstitution FOREIGN KEY (HealthInstitutionID) REFERENCES HealthInstitution(HealthInstitutionID) ON DELETE CASCADE,
             CONSTRAINT FK_FinancialInstitution FOREIGN KEY (FinancialInstitutionID) REFERENCES FinancialInstitution(FinancialInstitutionID) ON DELETE CASCADE,
@@ -213,15 +220,17 @@ def enableRLS():
     ALTER TABLE HealthInstitutionAcreditedInsurance ENABLE ROW LEVEL SECURITY;
     ALTER TABLE FinancialInstitution ENABLE ROW LEVEL SECURITY;
     -- ALTER TABLE financialinstitution ADD CONSTRAINT unique_financialinstitution_name UNIQUE (financialinstitutionname);
-    ALTER TABLE FinancialInstitutionNotes ENABLE ROW LEVEL SECURITY;
-    ALTER TABLE FinancialInstitutionBenefits ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE FinancialInstitutionNote ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE FinancialInstitutionBenefit ENABLE ROW LEVEL SECURITY;
     ALTER TABLE FinancialInstitutionProcess ENABLE ROW LEVEL SECURITY;
     ALTER TABLE BenefitsInclusionList ENABLE ROW LEVEL SECURITY;
     ALTER TABLE FinancialInstitutionRequirement ENABLE ROW LEVEL SECURITY;
-    ALTER TABLE Blogs ENABLE ROW LEVEL SECURITY;
-    ALTER TABLE BlogsRefLink ENABLE ROW LEVEL SECURITY;
-    ALTER TABLE Clinics ENABLE ROW LEVEL SECURITY;
-    ALTER TABLE ContactNumbers ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE Blog ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE BlogRefLink ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE Clinic ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE ContactNumber ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE Event ENABLE ROW LEVEL SECURITY;
+    ALTER TABLE EventOrganizer ENABLE ROW LEVEL SECURITY;
     """
 
     client.cursor.execute(enable_RLS)
@@ -232,50 +241,118 @@ def enableRLS():
 def injectPolicies():
 
     # Syntax:
-    # CREATE POLICY <Name_Of_Policy> ON <Table_Name>
-    #     FOR SELECT USING (true);
+    # CREATE POLICY <Name_Of_Policy> ON <Table_Name> FOR SELECT USING (true);
 
 
     policyInjectionQuery = """
     
-    -- Allow all users to SELECT (read) data
-    CREATE POLICY full_HealthInstitution_select ON HealthInstitution 
-    FOR SELECT USING (true);
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on HealthInstitution
+    CREATE POLICY full_HealthInstitution_select ON HealthInstitution FOR SELECT USING (true);
+    CREATE POLICY full_HealthInstitution_insert ON HealthInstitution FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_HealthInstitution_update ON HealthInstitution FOR UPDATE USING (true);
+    CREATE POLICY full_HealthInstitution_delete  ON HealthInstitution FOR DELETE USING (true);
     
-    -- Allow all users to INSERT (add) data
-    CREATE POLICY full_HealthInstitution_insert ON HealthInstitution 
-    FOR INSERT WITH CHECK (true);
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on FinancialInstitution
+    CREATE POLICY full_FinancialInstitution_select ON FinancialInstitution FOR SELECT USING (true);
+    CREATE POLICY full_FinancialInstitution_insert ON FinancialInstitution FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_FinancialInstitution_update ON FinancialInstitution FOR UPDATE USING (true);
+    CREATE POLICY full_FinancialInstitution_delete ON FinancialInstitution FOR DELETE USING (true);
     
-    -- Allow all users to UPDATE (modify) data
-    CREATE POLICY full_HealthInstitution_update ON HealthInstitution 
-    FOR UPDATE USING (true);
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on HealthInstitutionService
+    CREATE POLICY full_HealthInstitutionService_select ON HealthInstitutionService FOR SELECT USING (true);
+    CREATE POLICY full_HealthInstitutionService_insert ON HealthInstitutionService FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_HealthInstitutionService_update ON HealthInstitutionService FOR UPDATE USING (true);
+    CREATE POLICY full_HealthInstitutionService_delete ON HealthInstitutionService FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on HealthInstitutionAcreditedInsurance
+    CREATE POLICY full_HealthInstitutionAcreditedInsurance_select ON HealthInstitutionAcreditedInsurance FOR SELECT USING (true);
+    CREATE POLICY full_HealthInstitutionAcreditedInsurance_insert ON HealthInstitutionAcreditedInsurance FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_HealthInstitutionAcreditedInsurance_update ON HealthInstitutionAcreditedInsurance FOR UPDATE USING (true);
+    CREATE POLICY full_HealthInstitutionAcreditedInsurance_delete ON HealthInstitutionAcreditedInsurance FOR DELETE USING (true);
     
-    -- Allow all users to DELETE (remove) data
-    CREATE POLICY full_HealthInstitution_delete  ON HealthInstitution 
-    FOR DELETE USING (true);
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on FinancialInstitutionNote
+    CREATE POLICY full_FinancialInstitutionNote_select ON FinancialInstitutionNote FOR SELECT USING (true);
+    CREATE POLICY full_FinancialInstitutionNote_insert ON FinancialInstitutionNote FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_FinancialInstitutionNote_update ON FinancialInstitutionNote FOR UPDATE USING (true);
+    CREATE POLICY full_FinancialInstitutionNote_delete ON FinancialInstitutionNote FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on FinancialInstitutionBenefit
+    CREATE POLICY full_FinancialInstitutionBenefit_select ON FinancialInstitutionBenefit FOR SELECT USING (true);
+    CREATE POLICY full_FinancialInstitutionBenefit_insert ON FinancialInstitutionBenefit FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_FinancialInstitutionBenefit_update ON FinancialInstitutionBenefit FOR UPDATE USING (true);
+    CREATE POLICY full_FinancialInstitutionBenefit_delete ON FinancialInstitutionBenefit FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on FinancialInstitutionProcess
+    CREATE POLICY full_FinancialInstitutionProcess_select ON FinancialInstitutionProcess FOR SELECT USING (true);
+    CREATE POLICY full_FinancialInstitutionProcess_insert ON FinancialInstitutionProcess FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_FinancialInstitutionProcess_update ON FinancialInstitutionProcess FOR UPDATE USING (true);
+    CREATE POLICY full_FinancialInstitutionProcess_delete ON FinancialInstitutionProcess FOR DELETE USING (true);
     
-    -- Allow all users to SELECT (read) data
-    CREATE POLICY full_FinancialInstitution_select ON FinancialInstitution 
-    FOR SELECT USING (true);
-    
-    -- Allow all users to INSERT (add) data
-    CREATE POLICY full_FinancialInstitution_insert ON FinancialInstitution 
-    FOR INSERT WITH CHECK (true);
-    
-    -- Allow all users to UPDATE (modify) data
-    CREATE POLICY full_FinancialInstitution_update ON FinancialInstitution 
-    FOR UPDATE USING (true);
-    
-    -- Allow all users to DELETE (remove) data
-    CREATE POLICY full_FinancialInstitution_delete ON FinancialInstitution 
-    FOR DELETE USING (true);
-    
-    
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on FinancialInstitutionRequirement
+    CREATE POLICY full_FinancialInstitutionRequirement_select ON FinancialInstitutionRequirement FOR SELECT USING (true);
+    CREATE POLICY full_FinancialInstitutionRequirement_insert ON FinancialInstitutionRequirement FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_FinancialInstitutionRequirement_update ON FinancialInstitutionRequirement FOR UPDATE USING (true);
+    CREATE POLICY full_FinancialInstitutionRequirement_delete ON FinancialInstitutionRequirement FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on BenefitsInclusionList
+    CREATE POLICY full_BenefitsInclusionList_select ON BenefitsInclusionList FOR SELECT USING (true);
+    CREATE POLICY full_BenefitsInclusionList_insert ON BenefitsInclusionList FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_BenefitsInclusionList_update ON BenefitsInclusionList FOR UPDATE USING (true);
+    CREATE POLICY full_BenefitsInclusionList_delete ON BenefitsInclusionList FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on Blog
+    CREATE POLICY full_Blog_select ON Blog FOR SELECT USING (true);
+    CREATE POLICY full_Blog_insert ON Blog FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_Blog_update ON Blog FOR UPDATE USING (true);
+    CREATE POLICY full_Blog_delete ON Blog FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on BlogRefLink
+    CREATE POLICY full_BlogRefLink_select ON BlogRefLink FOR SELECT USING (true);
+    CREATE POLICY full_BlogRefLink_insert ON BlogRefLink FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_BlogRefLink_update ON BlogRefLink FOR UPDATE USING (true);
+    CREATE POLICY full_BlogRefLink_delete ON BlogRefLink FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on ContactNumber
+    CREATE POLICY full_ContactNumber_select ON ContactNumber FOR SELECT USING (true);
+    CREATE POLICY full_ContactNumber_insert ON ContactNumber FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_ContactNumber_update ON ContactNumber FOR UPDATE USING (true);
+    CREATE POLICY full_ContactNumber_delete ON ContactNumber FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on Journal
+    CREATE POLICY full_Journal_select ON Journal FOR SELECT USING (true);
+    CREATE POLICY full_Journal_insert ON Journal FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_Journal_update ON Journal FOR UPDATE USING (true);
+    CREATE POLICY full_Journal_delete ON Journal FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on JournalCategory
+    CREATE POLICY full_JournalCategory_select ON JournalCategory FOR SELECT USING (true);
+    CREATE POLICY full_JournalCategory_insert ON JournalCategory FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_JournalCategory_update ON JournalCategory FOR UPDATE USING (true);
+    CREATE POLICY full_JournalCategory_delete ON JournalCategory FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on Event
+    CREATE POLICY full_Event_select ON Event FOR SELECT USING (true);
+    CREATE POLICY full_Event_insert ON Event FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_Event_update ON Event FOR UPDATE USING (true);
+    CREATE POLICY full_Event_delete ON Event FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on EventOrganizer
+    CREATE POLICY full_EventOrganizer_select ON EventOrganizer FOR SELECT USING (true);
+    CREATE POLICY full_EventOrganizer_insert ON EventOrganizer FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_EventOrganizer_update ON EventOrganizer FOR UPDATE USING (true);
+    CREATE POLICY full_EventOrganizer_delete ON EventOrganizer FOR DELETE USING (true);
+
+    -- Allow all users to SELECT, INSERT, UPDATE, DELETE on Clinic
+    CREATE POLICY full_Clinic_select ON Clinic FOR SELECT USING (true);
+    CREATE POLICY full_Clinic_insert ON Clinic FOR INSERT WITH CHECK (true);
+    CREATE POLICY full_Clinic_update ON Clinic FOR UPDATE USING (true);
+    CREATE POLICY full_Clinic_delete ON Clinic FOR DELETE USING (true);
     """
 
     client.cursor.execute(policyInjectionQuery)
     client.conn.commit()
     print("RLS(Row Level Security) policy Injected successfully");
+    client.cursor.close()
 
 def dropTable():
     deleteTableQuery="""
@@ -283,15 +360,15 @@ def dropTable():
      DROP TABLE HealthInstitutionService;
      DROP TABLE HealthInstitutionAcreditedInsurance;
      DROP TABLE FinancialInstitution;
-     DROP TABLE FinancialInstitutionNotes;
-     DROP TABLE FinancialInstitutionBenefits;
+     DROP TABLE FinancialInstitutionNote;
+     DROP TABLE FinancialInstitutionBenefit;
      DROP TABLE FinancialInstitutionProcess;
      DROP TABLE BenefitsInclusionList;
      DROP TABLE FinancialInstitutionRequirement;
-     DROP TABLE Blogs;
-     DROP TABLE BlogsRefLink;
-     DROP TABLE Clinics;
-     DROP TABLE ContactNumbers;
+     DROP TABLE Blog;
+     DROP TABLE BlogRefLink;
+     DROP TABLE Clinic;
+     DROP TABLE ContactNumber;
     """
 
     client.cursor.execute(deleteTableQuery)
